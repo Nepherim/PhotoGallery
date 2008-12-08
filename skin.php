091814ec0ad1f7174bd06c2520d9dde464ad6213
David Gilbert <david@solidgone.com>
2008-12-08 00:33:51 +0000

    Clean up complete; layout done; added login/logout.

--- skin.php
+++ skin.php
@@ -21,18 +21,28 @@ $HTMLStylesFmt['pmwiki'] = '';
 ## Default color scheme
 global $SkinColor, $ValidSkinColors;
 if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
-array_push($ValidSkinColors, 'red', 'blue', 'orange', 'yellow', 'green');
+array_push($ValidSkinColors, 'red', 'blue', 'orange', 'yellow', 'green', 'lightblue');
 if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) ) {
 	$SkinColor = $_GET['color'];
 } elseif ( !in_array($SkinColor, $ValidSkinColors) ) {
 	$SkinColor = 'blue';
 }
 
-## Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
-$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
-Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
-
 ## Define a link stye for new page links
 global $LinkPageCreateFmt;
 SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
 
+## Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
+$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
+Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
+
+Markup('album', 'inline', "/\\(:album\\s*(.*?):\\)/se", "Keep(album(PSS('$1')))");
+function album ($args) {
+	$o = ParseArgs($args);
+	global $pagename;
+	return
+		'<div class="album">'
+		.'<div class="thumb">' .MakeLink($pagename,PSS($o['link']), '<img src="' .$o['thumb'] .'" />') .'</div>'
+		.'<div class="albumdesc">' .'<h3>' .MakeLink($pagename,PSS($o['link']), $o['title']) .'</h3><p>' .html_entity_decode($o['desc']) .'</p></div>'
+		.'</div>';
+}
