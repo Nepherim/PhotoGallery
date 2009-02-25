c79e911ce4020654cb6bfdd6d75e4ca65f2d439b
David Gilbert <david@solidgone.com>
2009-02-25 04:09:59 +0000

    chg: Standardized tabs, navigation, footer naming, and search order. chg: Formated skin.php to standard skin coding (colors, linkfmt). fix: Standardized skin identification text in footer.

--- skin.php
+++ skin.php
@@ -1,11 +1,8 @@
 <?php if (!defined('PmWiki')) exit();
-/*
- * PmWiki PhotoGallery skin
- * Version 0.3.0  (21-Dec-2008)
- * @requires PmWiki 2.2
+/* PmWiki PhotoGallery skin
  *
- * Examples at: http://pmwiki.com/Cookbook/PhotoGallery and http://skins.solidgone.com/PhotoGallery
- * Copyright (c) 2007 David Gilbert
+ * Examples at: http://pmwiki.com/Cookbook/PhotoGallery and http://solidgone.com/Skins/
+ * Copyright (c) 2009 David Gilbert
  * Dual licensed under the MIT and GPL licenses:
  *    http://www.opensource.org/licenses/mit-license.php
  *    http://www.gnu.org/licenses/gpl.html
@@ -14,11 +11,21 @@ global $FmtPV;
 $FmtPV['$SkinName'] = '"PhotoGallery"';
 $FmtPV['$SkinVersion'] = '"0.3.0"';
 
-## Override pmwiki styles otherwise they will override styles declared in css
-global $HTMLStylesFmt;
-$HTMLStylesFmt['pmwiki'] = '';
+## Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
+$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
+Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
+Markup('album', 'inline', "/\\(:album\\s*(.*?):\\)/se", "Keep(album(PSS('$1')))");
 
-## Default color scheme
+# ----------------------------------------
+# - Standard Skin Setup
+# ----------------------------------------
+$FmtPV['$WikiTitle'] = '$GLOBALS["WikiTitle"]';
+
+# Define a link stye for new page links
+global $LinkPageCreateFmt;
+SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
+
+# Default color scheme
 global $SkinColor, $ValidSkinColors;
 if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
 array_push($ValidSkinColors, 'red', 'blue', 'orange', 'yellow', 'green', 'lightblue');
@@ -28,22 +35,20 @@ if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) ) {
 	$SkinColor = 'blue';
 }
 
-## Add a custom page storage location
-global $PageStorePath, $WikiLibDirs;
+# Override pmwiki styles otherwise they will override styles declared in css
+global $HTMLStylesFmt;
+$HTMLStylesFmt['pmwiki'] = '';
+
+# Add a custom page storage location
+global $WikiLibDirs;
 $PageStorePath = dirname(__FILE__)."/wikilib.d/{\$FullName}";
 $where = count($WikiLibDirs);
 if ($where>1) $where--;
 array_splice($WikiLibDirs, $where, 0, array(new PageStore($PageStorePath)));
 
-## Define a link stye for new page links
-global $LinkPageCreateFmt;
-SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
-
-## Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
-$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
-Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
-
-Markup('album', 'inline', "/\\(:album\\s*(.*?):\\)/se", "Keep(album(PSS('$1')))");
+# ----------------------------------------
+# - Skin Functions
+# ----------------------------------------
 function album ($args) {
 	$o = ParseArgs($args);
 	global $pagename;
